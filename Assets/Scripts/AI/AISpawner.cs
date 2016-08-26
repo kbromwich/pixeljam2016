b8e504b0dffaee1338dc3787bda390fa8fd161ef
NewZulund <colias@live.com>
2016-08-27 11:42:39 +1200

    Added flashing lights & enemy spawners

---
+++ Assets/Scripts/AI/AISpawner.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AISpawner : MonoBehaviour {
+
+    public GameObject ItemToSpawn;
+    public int SpawnCount = 200;
+    public float SpawnTime = 10.0f;
+
+    float TimeSinceLastSpawn = 0.0f;
+    float TimeBetweenSpawns = 0.0f;
+    int SpawnsLeft = 0;
+
+    void Start()
+    {
+        TimeBetweenSpawns = SpawnTime / SpawnCount;
+        SpawnsLeft = SpawnCount;
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+        if(SpawnsLeft > 0)
+        {
+            TimeSinceLastSpawn += Time.deltaTime;
+
+            if (TimeSinceLastSpawn > TimeBetweenSpawns)
+            {
+                //Spawn shit
+                GameObject spawnedGameObject = Instantiate<GameObject>(ItemToSpawn);
+                spawnedGameObject.transform.position = transform.position;
+                TimeSinceLastSpawn = 0;
+                --SpawnsLeft;
+            }
+        }
+    }
+       
+}
---
+++ Assets/Scripts/Lights/FlashingLight.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(Light))]
+public class FlashingLight : MonoBehaviour
+{
+
+    public float FlashRate = 1.0f;
+
+    float NextLightChange = 0.0f;
+    bool LightOn = false;
+
+    Light light;
+    float LightIntensity;
+
+    void Start()
+    {
+        light = GetComponent<Light>();
+        LightIntensity = light.intensity;
+    }
+
+    void Update()
+    {
+        if (NextLightChange < Time.time)
+        {
+            LightOn = !LightOn;
+
+            light.intensity = LightOn ? 0 : LightIntensity;
+            NextLightChange = Time.time + FlashRate;
+        }
+    }
+}
+
