727a69495fb7800ef77300cd16b6b05c07eba4fd
Isaac <isaac.blomfield@gmail.com>
2016-08-27 00:39:33 +1200

    Moved some files around

---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttack : MonoBehaviour {
+
+    public float AttackReach = 2.0f;
+
+	// Update is called once per frame
+	void Update () {
+
+        //Player has attacked
+	    if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack attack = GetComponent<Attack>();
+
+            if(attack)
+            {
+                //Check if player is looking at someone with health.
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, transform.forward, out hit, AttackReach))
+                {
+                    //Damage the health
+                    Health health = hit.collider.GetComponent<Health>();
+                    if(health != null)
+                    {
+                        attack.MakeAttack(health);
+                    }
+                   
+                }
+                    
+            }
+            else
+            {
+                print("Player doesn't have an attack class");
+            }
+        }
+	}
+}
