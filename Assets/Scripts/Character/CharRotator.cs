17e23190efac231e7a650bab499f750431297269
NewZulund <colias@live.com>
2016-08-27 01:43:59 +1200

    Added 2D rotator to players. Needs to be added to AI

---
+++ Assets/Scripts/Character/CharRotator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharRotator : MonoBehaviour {
+
+    Rigidbody RigidB;
+
+    void Start()
+    {
+        RigidB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+	void Update ()
+    {
+        if (RigidB != null)
+        {
+            if(RigidB.velocity.z > 0)
+            {
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+            }
+            else
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+        }
+
+        Debug.DrawRay(transform.position, transform.forward, Color.red, 10);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerDeath.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDeath : MonoBehaviour {
+
+    Health health;
+    bool HasDied = false;
+
+	// Use this for initialization
+	void Start () {
+        health = GetComponent<Health>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	    if(!HasDied && health != null && health.health < 0)
+        {
+            HasDied = true;
+            //TODO implement deaths something?
+            Destroy(gameObject);
+        }
+	}
+}
