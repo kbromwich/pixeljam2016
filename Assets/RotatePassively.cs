8d53f987f076a50f3d88bcb407505d7bddb2f5ea
DiglDixon <digl.design.nz@gmail.com>
2016-08-28 11:27:34 +1200

    Decked out the scene with doodads

---
+++ Assets/RotatePassively.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatePassively : MonoBehaviour {
+
+	public Vector3 passiveRotation;
+
+	void FixedUpdate(){
+		transform.rotation = Quaternion.Euler (transform.rotation.eulerAngles + passiveRotation);
+	}
+}
