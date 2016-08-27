6e1cff219a9536efc36bb9336de048ed2f31442c
NewZulund <colias@live.com>
2016-08-27 22:46:40 +1200

    Fixed colliders on head rods

--- Assets/Scripts/Digl/HornAttack.cs
+++ Assets/Scripts/Digl/HornAttack.cs
@@ -3,10 +3,19 @@ using System.Collections;
 [RequireComponent(typeof(Attack))]
 public class HornAttack : MonoBehaviour {
 
-	private void OnTriggerEnter(Collider c){
+    public GameObject Emitter;
+    public Health HealthToAvoid;
+
+    void Awake()
+    {
+        Emitter = GameObject.Instantiate(Resources.Load<GameObject>("Sparks"));
+    }
+
+	private void OnCollisionEnter(Collision c){
         HealthHitbox health = c.gameObject.GetComponent<HealthHitbox>();
 
-		if (health) {
+		if (health && health != HealthToAvoid) {
+            Instantiate(Emitter, c.transform.position, c.transform.rotation);
             Attack attackComp = GetComponent<Attack>();
             attackComp.MakeAttack(health.health);
 		}
