52c337960ecba015be86d0a6f565fee6dc30b22a
Kohl Bromwich <kohl.bromwich@gmail.com>
2016-08-27 13:41:20 +1200

    Got the stupid menu working

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -2,61 +2,87 @@
 using System.Collections;
 using System.Collections.Generic;
 using System;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    List<KeyValuePair<string, Action>> menuOptions = new List<KeyValuePair<string, Action>>();
+    List<MenuItem> menuOptions = new List<MenuItem>();
     int selectedIndex = 0;
-    bool lastFrameInput = false;
+    int xinput = 0;
+    int yinput = 0;
+    float lastxinput = 0;
+    float lastyinput = 0;
     bool submit = false;
+    
+    MenuItem players;
+    MenuItem bots;
 
     // Use this for initialization
     void Start()
     {
-        menuOptions.Add(new KeyValuePair<string, Action>("Play", Play));
-        menuOptions.Add(new KeyValuePair<string, Action>("Stuff", Stuff));
-        menuOptions.Add(new KeyValuePair<string, Action>("Exit", Exit));
+        var play = new MenuItem("Play");
+        play.action = Play;
+
+        players = new MenuItem("Players");
+        for (int i = 1; i <= 4; i++) {
+            var opt = new MenuOption(i.ToString());
+            players.options.Add(opt);
+        }
+
+        bots = new MenuItem("Bots");
+        for (int i = 1; i <= 4; i++)
+        {
+            var opt = new MenuOption(i.ToString());
+            bots.options.Add(opt);
+        }
+
+        var exit = new MenuItem("Exit");
+        exit.action = Exit;
+
+        menuOptions.Add(play);
+        menuOptions.Add(players);
+        menuOptions.Add(bots);
+        menuOptions.Add(exit);
     }
 
     void Update()
     {
+        yinput = 0;
+        xinput = 0;
         float verticalAxis = Input.GetAxis("Vertical");
-
-        if (verticalAxis < 0 && !lastFrameInput)
+        if (lastyinput == 0 && verticalAxis != 0)
         {
-            selectedIndex += 1;
+            yinput = Math.Sign(verticalAxis);
         }
-        else if (verticalAxis > 0 && !lastFrameInput)
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        if (lastxinput == 0 && horizontalAxis != 0)
         {
-            selectedIndex -= 1;
-        }
-
-        if (selectedIndex < 0)
-        {
-            selectedIndex = menuOptions.Count - 1;
-        }
-        else if (selectedIndex >= menuOptions.Count)
-        {
-            selectedIndex = 0;
+            xinput = Math.Sign(horizontalAxis);
         }
 
+        selectedIndex = Mod(selectedIndex - yinput, menuOptions.Count);
         submit = Input.GetButtonDown("Submit");
-        lastFrameInput = verticalAxis != 0;
+
+        menuOptions[selectedIndex].Update(xinput);
+
+        lastxinput = horizontalAxis;
+        lastyinput = verticalAxis;
     }
 
     void Play()
     {
         print("Play!");
-    }
+        int numPlayers = int.Parse(players.SelectedOption());
+        int numBots = int.Parse(bots.SelectedOption());
 
-    void Stuff()
-    {
-        print("Stuff!");
+        print("Loading scene Main");
+        SceneManager.LoadScene("Main");
     }
 
     void Exit()
     {
         print("Exit!");
+        Application.Quit();
     }
 
     void OnGUI()
@@ -66,17 +92,85 @@ public class MainMenu : MonoBehaviour
         for (int i = 0; i < menuOptions.Count; i++)
         {
             var option = menuOptions[i];
-            GUI.SetNextControlName(option.Key);
-            GUI.Button(new Rect(10, y, 200, 30), option.Key);
-            if (i == selectedIndex)
+            option.Draw(y, i == selectedIndex, submit);
+            y += 30;
+        }
+    }
+
+    public static int Mod(int x, int m)
+    {
+        return (x % m + m) % m;
+    }
+
+    class MenuItem
+    {
+        public string name = "Lalala";
+        public Action action = null;
+        public int selectedOption = 0;
+        public List<MenuOption> options = new List<MenuOption>();
+
+        public MenuItem(string name)
+        {
+            this.name = name;
+        }
+
+        public void Update(int xinput)
+        {
+            if (options.Count > 0)
             {
-                GUI.FocusControl(option.Key);
-                if (submit)
+                selectedOption = Mod(selectedOption + xinput, options.Count);
+            }
+        }
+
+        public void Draw(int y, bool selected, bool submit)
+        {
+            GUI.SetNextControlName(name);
+
+            bool clicked = false;
+            //if (action != null) {
+                clicked = GUI.Button(new Rect(10, y, 200, 30), name);
+            //}
+            //else
+            //{
+                //GUI.Label(new Rect(10, y, 200, 30), name);
+            //}
+
+            if (clicked || selected)
+            {
+                GUI.FocusControl(name);
+                if (submit || clicked)
                 {
-                    option.Value();
+                    if (action != null)
+                    {
+                        action.Invoke();
+                    }
                 }
             }
-            y += 30;
+
+            if (options.Count > 0)
+            {
+                options[selectedOption].Draw(y);
+            }
+        }
+
+        public string SelectedOption()
+        {
+            return options[selectedOption].name;
+        }
+    }
+
+    class MenuOption
+    {
+        public string name = "opt";
+
+        public MenuOption(string name)
+        {
+            this.name = name;
+        }
+
+        public void Draw(int y)
+        {
+            GUI.Label(new Rect(250, y, 200, 30), name);
         }
     }
 }
