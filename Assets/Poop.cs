bf1a18229c83bcdfa9ffead9184a5602d0663a06
DiglDixon <digl.design.nz@gmail.com>
2016-08-27 15:38:37 +1200

    Added Horn. And Poop.
    Untested

---
+++ Assets/Poop.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(SphereCollider))]
+public class Poop : MonoBehaviour {
+
+	private float countdown = 3f;
+
+	private float endRadius = 20f;
+
+	private float radiusExpansionRate = 80f;
+
+	private float explosionForce = 10000f;
+
+	private SphereCollider col;
+
+	// Use this for initialization
+	void Start () {
+		col = GetComponent<SphereCollider> ();
+		StartCoroutine (ExplosionCountdown ());
+	}
+
+	private IEnumerator ExplosionCountdown(){
+		yield return new WaitForSeconds (countdown);
+		Explode ();
+	}
+
+	private void Explode(){
+		StartCoroutine (RunExplsoion ());
+	}
+
+	private IEnumerator RunExplsoion(){
+		float radius = 0f;
+		while (radius < endRadius) {
+			radius += radiusExpansionRate * Time.deltaTime;
+			col.radius = radius;
+			yield return null;
+		}
+		Destroy (gameObject);
+	}
+
+	void OnTriggerEnter(Collider c){
+		Rigidbody body = c.GetComponent<Rigidbody> ();
+		if (body) {
+			body.AddExplosionForce (explosionForce, transform.position, endRadius);		
+		}
+	}
+}
---
+++ Assets/Scripts/Digl/HealthHitbox.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthHitbox : MonoBehaviour {
+
+	public Health health;
+
+}
