ab1867916e1beda1ca98cae8311d1513c711d82e
Kohl Bromwich <kohl.bromwich@gmail.com>
2016-08-28 14:45:57 +1200

    Prep for camera work

--- Assets/Scripts/Game/GameController.cs
+++ Assets/Scripts/Game/GameController.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
-    
+
+    public CameraControl cameraControl;
+
     List<Player> Players;
 
     public string[] Names;
@@ -13,10 +15,22 @@ public class GameController : MonoBehaviour {
     void Start()
     {
         Players = new List<Player>(FindObjectsOfType<Player>());
+
         foreach(Player player in Players)
         {
             player.name = Names[player.Index];
 			player.GetComponent<MeshColourer>().SetMaterialColour(Colors[player.Index]);
+        }
+
+        UpdateCameraTargets();
+    }
+
+    private void UpdateCameraTargets()
+    {
+        Transform[] targets = new Transform[Players.Count];
+        for (int i = 0; i < Players.Count; i++)
+        {
+            targets[i] = Players[i].transform;
         }
     }
 
@@ -31,5 +45,7 @@ public class GameController : MonoBehaviour {
             EndGameUIController item = FindObjectOfType<EndGameUIController>();
             item.PlayerWon(Players[0].name);
         }
+
+        UpdateCameraTargets();
     }
 }
