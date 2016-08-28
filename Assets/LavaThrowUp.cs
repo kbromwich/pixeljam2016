7d136c77444981d4cb3ddb746b39875808cb5b45
NewZulund <colias@live.com>
2016-08-28 12:53:00 +1200

    Added lava to scene. Added lava damaging code

---
+++ Assets/LavaThrowUp.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LavaThrowUp : MonoBehaviour {
+
+    public  Vector3 ThrowForce = Vector3.up * 1000;
+    public float damage = 0.5f;
+
+	void OnTriggerEnter(Collider collider)
+    {
+        print("InTriggerEnter");
+        Rigidbody rb = collider.gameObject.GetComponent<Rigidbody>();
+        if(rb)
+        {
+            rb.AddForce(ThrowForce);
+            print("throwing");
+        }
+
+        Health health = collider.gameObject.GetComponent<Health>();
+        if (health)
+        {
+            health.TakeDamage(damage);
+        }
+
+        HealthHitbox healthHitbox = collider.gameObject.GetComponent<HealthHitbox>();
+        if (healthHitbox)
+        {
+            healthHitbox.health.TakeDamage(damage);
+        }
+    }
+}
